809f908fe5054e8a1a737141210b0047ae1d7df2
tyccronin <tyccronin@gmail.com>
2022-02-15 12:48:47 -0800

    Implementation

---
+++ Binary Search/Program.cs
@@ -0,0 +1,44 @@
+﻿/* 
+This program completes an iterative Binary Search
+*/
+
+using System;
+
+class GUtil {
+    // Returns an index of x if it is present in arr[], else return -1
+    static int binarySearch(int[] arr, int x){
+        
+        int i = 0;
+        int arrayLength = arr.Length - 1; 
+       
+        while (i <= arrayLength){
+            
+            int m = i + ((arrayLength)-i)/2;
+    
+            if(arr[m] == x){
+                return m; // Check if x is present at mid
+            }
+            if(arr[m] < x){
+                i = m + 1; // If x is greater, ignore left half
+            } else {
+                arrayLength = m-1;  // If x is smaller, ignore right half
+            }
+        }
+        // If we get here, then the element was not present!
+        return -1;
+    }
+
+    // Driver Code
+    public static void Main(string[] args){
+        int[] arr = {2, 3, 4, 10, 40};
+        int n = arr.Length;
+        int x = 10;
+        int result = binarySearch(arr, x);
+        if (result == -1){
+            Console.WriteLine("Element not present!");
+        } else {
+            Console.WriteLine("Element found at " + "index " + result);
+        }
+    }
+}
+
